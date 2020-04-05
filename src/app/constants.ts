45b697211cfda0c6983992af77ff3d75381e6c8d
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-05 21:06:32 +0300

    init common structure

---
+++ src/app/constants.ts
@@ -0,0 +1,10 @@
+export class Constants {
+
+    public static readonly SECTION = Object.freeze({
+        empty: '',
+        root: '/',
+        dashboard: 'dashboard'
+    });
+
+    public static readonly PATH = Object.freeze({});
+}
