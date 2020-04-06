7dde41963161e86203a710f207a48fd9eb309320
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-06 15:37:50 +0300

    update paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {Constants} from './constants';
 import {ContentLayoutComponent} from './layouts/content-layout/content-layout.component';
 import {DashboardPageComponent} from './pages/dashboard-page/dashboard-page.component';
+import {SettingsPageComponent} from './pages/settings-page/settings-page.component';
 
 const routes: Routes = [
         {
-            path: '', redirectTo: Constants.PATH.dashboard, pathMatch: 'full'
-        },
-        {
-            path: Constants.PATH.dashboard, component: ContentLayoutComponent,
-            children: [
-                {
-                    path: '', component: DashboardPageComponent
-                }
+            path: '', component: ContentLayoutComponent, children: [
+                {path: '', redirectTo: Constants.PATH.dashboard, pathMatch: 'full'},
+                {path: Constants.PATH.dashboard, component: DashboardPageComponent},
+                {path: Constants.PATH.settings, component: SettingsPageComponent}
             ]
         }
     ]
