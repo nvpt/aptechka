28e3b250f94f8daf1e368972ea81c8b6ad83c6a3
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-06 15:38:14 +0300

    put menu content configs in constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,12 +1,35 @@
+export interface menuItem {
+    title: string;
+    path: string;
+    nested?: boolean;
+    selected?: boolean;
+    children?: menuItem[];
+}
+
 export class Constants {
 
     public static readonly SECTION = Object.freeze({
         empty: '',
         root: '/',
-        dashboard: 'dashboard'
+        dashboard: 'dashboard',
+        settings: 'settings',
     });
 
     public static readonly PATH = Object.freeze({
-        dashboard: Constants.SECTION.dashboard
+        dashboard: Constants.SECTION.dashboard,
+        settings: Constants.SECTION.settings
     });
+
+    public static readonly menuItems: menuItem[] = [
+        {
+            title: 'Dashboard',
+            path: Constants.PATH.dashboard,
+            selected: true
+        },
+        {
+            title: 'Settings',
+            path: Constants.PATH.settings,
+            selected: false
+        },
+    ];
 }
