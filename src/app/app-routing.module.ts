bd39beb76ad617b7a207d598bb89a9367ed19cfd
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-09 00:57:20 +0300

    temp put About component into Settings PAge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,19 @@ import {Constants} from './constants';
 import {ContentLayoutComponent} from './layouts/content-layout/content-layout.component';
 import {DashboardPageComponent} from './pages/dashboard-page/dashboard-page.component';
 import {SettingsPageComponent} from './pages/settings-page/settings-page.component';
+import {AboutComponent} from './pages/about/about.component';
 
 const routes: Routes = [
         {
             path: '', component: ContentLayoutComponent, children: [
                 {path: '', component: DashboardPageComponent, pathMatch: 'full'},
-                {path: Constants.PATH.settings, component: SettingsPageComponent}
+                {
+                    path: Constants.PATH.settings, component: SettingsPageComponent, children: [
+                        {
+                            path: Constants.SECTION.about, component: AboutComponent
+                        }
+                    ]
+                }
             ]
         }
     ]
