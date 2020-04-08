0854c446c37203e0fd5923ab0ba5e7f9d119c6e2
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-08 15:11:32 +0300

    add @ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ColorThemeType, ThemesService} from './modules/themes/themes.service';
 import {Constants} from './constants';
 import {StorageDataI} from './interfaces/storage-data-interface';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
     selector: 'app-root',
@@ -9,10 +10,12 @@ import {StorageDataI} from './interfaces/storage-data-interface';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+    title: string = 'aptechka';
     menuBg: string = 'var(--color-app_bg-bg)';
 
-    constructor(private themeService: ThemesService) {
-
+    constructor(private themeService: ThemesService, private translate: TranslateService) {
+        translate.setDefaultLang('en');
+        translate.use('en');
     }
 
     ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 import {MenuModule} from './modules/menu/menu.module';
 
@@ -11,8 +13,13 @@ import {DashboardPageComponent} from './pages/dashboard-page/dashboard-page.comp
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {SharedModule} from './shared/shared.module';
-import { MenuContentComponent } from './components/menu-content/menu-content.component';
-import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import {MenuContentComponent} from './components/menu-content/menu-content.component';
+import {SettingsPageComponent} from './pages/settings-page/settings-page.component';
+import {HttpClient} from '@angular/common/http';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
     declarations: [
@@ -27,7 +34,16 @@ import { SettingsPageComponent } from './pages/settings-page/settings-page.compo
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule,
         MenuModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            },
+            defaultLanguage: 'en'
+        }),
         SharedModule
     ],
     providers: [],
