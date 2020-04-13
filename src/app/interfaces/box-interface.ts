fdf2558f6ec3364984e1f690790876a7152ba10f
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-13 15:54:00 +0300

    box interface should bu updated

--- src/app/interfaces/box-interface.ts
+++ src/app/interfaces/box-interface.ts
@@ -1,10 +1,13 @@
+import {MedicamentI} from './medicament-interface';
+//todo *** in progress
 export interface BoxI {
     title: string;
     place?: string;
     description?: string;
     img?: string;
     groups?: string[];
-    total: number;
+    medicaments?: MedicamentI[];
+    total?: number;
     onTheVerge?: number;
     overdue?: number;
     alert?: number;
