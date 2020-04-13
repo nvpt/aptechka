17f866957e494c79952d5eaf82faac4dc406ab4f
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-13 22:03:37 +0300

    target group interface + mock done

---
+++ src/app/pages/target-groups-page/target-groups-mock.ts
@@ -0,0 +1,36 @@
+import {TargetGroupI} from '../../interfaces/target-group-interface';
+
+export const targetGroupsMock: TargetGroupI[] = [
+    {
+        id: 1,
+        title: "От головы"
+    },
+    {
+        id: 2,
+        title: "От горла"
+    },
+    {
+        id: 3,
+        title: "От живота"
+    },
+    {
+        id: 4,
+        title: "Обезболивающее"
+    },
+    {
+        id: 5,
+        title: "Слабительное"
+    },
+    {
+        id: 6,
+        title: "Антисептик"
+    },
+    {
+        id: 7,
+        title: "Успокоительное"
+    },
+    {
+        id: 8,
+        title: "От аллергии"
+    },
+]
