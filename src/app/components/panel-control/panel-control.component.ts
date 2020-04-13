90aab9f545668acbacc72587ea474cedb58ed0a9
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-13 20:28:46 +0300

    panel-control improvements

--- src/app/components/panel-control/panel-control.component.ts
+++ src/app/components/panel-control/panel-control.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-panel-control',
-  templateUrl: './panel-control.component.html',
-  styleUrls: ['./panel-control.component.scss']
+    selector: 'app-panel-control',
+    templateUrl: './panel-control.component.html',
+    styleUrls: ['./panel-control.component.scss']
 })
 export class PanelControlComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('leftPart', {static: true}) leftPart: ElementRef;
+    @ViewChild('centerPart', {static: true}) centerPart: ElementRef;
+    @ViewChild('rightPart', {static: true}) rightPart: ElementRef;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    ngOnInit(): void {
+        this.checkChildren(this.leftPart);
+        this.checkChildren(this.centerPart);
+        this.checkChildren(this.rightPart);
+    }
+
+    checkChildren(element: ElementRef): void {
+        if (element.nativeElement.childNodes.length < 2) {
+            element.nativeElement.classList.add('hide');
+        }
+    }
 }
