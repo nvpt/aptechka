13380d6767dc0e31cfcf6cc9492918682120e887
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-13 21:11:14 +0300

    medicament interface + mock in progress

--- src/app/interfaces/medicament-interface.ts
+++ src/app/interfaces/medicament-interface.ts
@@ -1,2 +1,8 @@
 export interface MedicamentI {
+    title: string;
+    issueDate: Date | string;
+    expiryDate: Date | string;
+    placeId?: number;
+    impactTypeId?: number;
+    targetGroupsId?: number[];
 }
---
+++ src/app/pages/medicaments-page/medicaments.mock.ts
@@ -0,0 +1,12 @@
+import {MedicamentI} from '../../interfaces/medicament-interface';
+
+export const medicamentsMock: MedicamentI[] = [
+    {
+        title: "Анальгин",
+        issueDate: "",
+        expiryDate: "",
+        impactTypeId: 2,
+        placeId: 1,
+        targetGroupsId: [1,2,5]
+    }
+]
