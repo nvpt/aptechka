a14bddf2d7bcbab382926baf81eec5db871c18b6
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-13 22:13:02 +0300

    places group interface + mock done

--- src/app/interfaces/box-interface.ts
+++ src/app/interfaces/box-interface.ts
@@ -1,14 +1,20 @@
 import {MedicamentI} from './medicament-interface';
+import {TargetGroupI} from './target-group-interface';
+import {PlaceI} from './place-interface';
 //todo *** in progress
 export interface BoxI {
     title: string;
-    place?: string;
+    place?: PlaceI;
     description?: string;
     img?: string;
-    groups?: string[];
-    medicaments?: MedicamentI[];
+    targetGroups?: TargetGroupI[];
+
+    medicamentsId?: number[];
     total?: number;
     onTheVerge?: number;
     overdue?: number;
     alert?: number;
+}
+export interface NewBoxI extends BoxI {
+    newImg?: File;
 }
