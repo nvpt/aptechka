18a279264c0093d1f8e9a987884dcf4a9b3afebe
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-13 22:24:08 +0300

    impact-type group interface + mock done

---
+++ src/app/interfaces/impact-type-interface.ts
@@ -0,0 +1,4 @@
+export interface ImpactTypeI {
+    id:number;
+    title:string;
+}
---
+++ src/app/pages/impact-types-page/impact-types-mock.ts
@@ -0,0 +1,28 @@
+import {ImpactTypeI} from '../../interfaces/impact-type-interface';
+
+export const impactTypesMock: ImpactTypeI[] = [
+    {
+        id: 1,
+        title: 'Обезболивающее'
+    },
+    {
+        id: 2,
+        title: 'Слабительное'
+    },
+    {
+        id: 3,
+        title: 'Антисептик'
+    },
+    {
+        id: 4,
+        title: 'Успокоительное'
+    },
+    {
+        id: 5,
+        title: 'Антигистаминное'
+    },
+    {
+        id: 6,
+        title: 'Перевязочные'
+    }
+];
