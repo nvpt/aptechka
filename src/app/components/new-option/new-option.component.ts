01381b1b07fc681f82fe97435d9f2f4a585f2477
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-14 22:25:14 +0300

    close new option component by esc

--- src/app/components/new-option/new-option.component.ts
+++ src/app/components/new-option/new-option.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-new-option',
@@ -23,13 +23,17 @@ export class NewOptionComponent implements OnInit {
             this.opened = true;
             setTimeout(() => {
                 this.inputField && this.inputField.nativeElement.focus();
-            }, 0)
+            }, 0);
         }
 
     }
 
     cancelCreation(event: KeyboardEvent | MouseEvent) {
-        if (event.type === 'keydown' && (<KeyboardEvent>event).key === 'Enter' ||
+        if (event.type === 'keydown' &&
+            (
+                ((<KeyboardEvent>event).key === 'Enter') ||
+                ((<KeyboardEvent>event).key === 'Escape')
+            ) ||
             event.type === 'click') {
 
             this.opened = false;
@@ -40,7 +44,7 @@ export class NewOptionComponent implements OnInit {
         if (event.type === 'keydown' && (<KeyboardEvent>event).key === 'Enter' ||
             event.type === 'click') {
 
-            const inputValue = this.inputField.nativeElement.value
+            const inputValue = this.inputField.nativeElement.value;
             this.onAdd.emit(inputValue);
             this.opened = false;
         }
