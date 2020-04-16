27879b15005b457c98683319caae4a91219ba5e0
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-16 23:39:33 +0300

    medicaments-page.component - in progress

--- src/app/pages/medicaments-page/medicaments-page.component.ts
+++ src/app/pages/medicaments-page/medicaments-page.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {MedicamentsService} from '../../services/medicaments.service';
 import {BoxI} from '../../interfaces/box-interface';
 import {BoxesService} from '../../services/boxes.service';
+import {Constants} from '../../constants';
+import {StorageDataI} from '../../interfaces/storage-data-interface';
 
 @Component({
     selector: 'app-medicaments-page',
@@ -9,13 +11,20 @@ import {BoxesService} from '../../services/boxes.service';
     styleUrls: ['./medicaments-page.component.scss']
 })
 export class MedicamentsPageComponent implements OnInit {
+    currentLanguage: string = Constants.DEFAULT_LANGUAGE;
 
     constructor(public medicamentsService: MedicamentsService, private boxesService: BoxesService) {
     }
 
     ngOnInit(): void {
+        this.defineLanguage();
     }
 
+    defineLanguage(): void {
+        const storageData = localStorage.getItem(Constants.STORAGE_KEY);
+        const storageObj: StorageDataI = storageData ? JSON.parse(storageData) : {};
+        this.currentLanguage = storageObj.lang ? storageObj.lang : this.currentLanguage;
+    }
 
     defineBox(boxId: number): BoxI {
         return this.boxesService.boxes.find(box => box.id === boxId);
