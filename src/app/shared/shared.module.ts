482a0cd7934bb72a5d442f24b08f67a3e9f3ef39
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-16 23:39:20 +0300

    added ru locale on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+import {registerLocaleData} from '@angular/common';
+import ruLocale from '@angular/common/locales/ru';
+
+registerLocaleData(ruLocale, 'ru');
+
 @NgModule({
     imports: [
         CommonModule,
