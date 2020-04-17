1697cd136daa2095f84cb7e6fd5107a36a231ada
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-17 17:19:25 +0300

    made edit-box component (as copy of new box component)

---
+++ src/app/pages/edit-box/edit-box.component.ts
@@ -0,0 +1,110 @@
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+
+import {Constants} from '../../constants';
+
+import {MenuService} from '../../modules/menu/menu-services/menu.service';
+import {TargetGroupsService} from '../../services/target-groups.service';
+import {BoxesService} from '../../services/boxes.service';
+import {TargetGroupI} from '../../interfaces/target-group-interface';
+import {NewBoxI} from '../../interfaces/box-interface';
+
+
+@Component({
+    selector: 'app-edit-box',
+    templateUrl: './edit-box.component.html',
+    styleUrls: ['./edit-box.component.scss']
+})
+export class EditBoxComponent implements OnInit, OnDestroy {
+    @ViewChild('titleInput') titleInput: ElementRef;
+
+    form: FormGroup;
+    imgUrl: string;
+    targetGroups: TargetGroupI[] = [];
+
+    constructor(public targetGroupsService: TargetGroupsService, private location: Location, private menuService: MenuService, private boxesService: BoxesService,
+        private router: Router) {
+    }
+
+    ngOnInit(): void {
+        this.menuService.hide();
+        this.initForm();
+        this.getTargetGroups();
+        setTimeout(() => {
+            this.titleInput && this.titleInput.nativeElement.focus();
+        }, 0);
+    }
+
+    initForm() {
+        this.form = new FormGroup({
+            title: new FormControl('', [Validators.required]),
+            description: new FormControl(''),
+            img: new FormControl(null),
+            medicaments: new FormControl([]),
+        });
+    }
+
+    ngOnDestroy() {
+        this.menuService.show();
+    }
+
+    cancelAdding() {
+        this.location.back();
+    }
+
+    addImage(event: Event) {
+        const img = (<HTMLInputElement>(event.target)).files[0];
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            this.imgUrl = reader.result as string;
+        };
+        img && reader.readAsDataURL(img);
+
+        this.form.patchValue({img});
+    }
+
+    clearPreview() {
+        this.form.controls.img.reset();
+        this.imgUrl = null;
+        this.form.controls.img.updateValueAndValidity();
+    }
+
+
+    /*Target groups*/
+    getTargetGroups() {
+        this.targetGroupsService.getTargetGroups();
+    }
+
+    toggleTargetGroup(event: MouseEvent, targetGroup: TargetGroupI) {
+
+        const checked: boolean = (<HTMLInputElement>event.target).checked;
+
+        if (checked) {
+            if (this.targetGroups.every((group) => (group.id !== targetGroup.id))) {
+                this.targetGroups.push(targetGroup);
+            }
+        } else {
+            this.targetGroups = [...this.targetGroups.filter((group) => (group.id !== targetGroup.id))];
+        }
+    }
+
+
+    saveBox() {
+        const box: NewBoxI = {
+            id: new Date().getTime(),
+            description: this.form.value.description,
+            title: this.form.value.title,
+            newImg: this.form.value.img,
+            img: this.imgUrl,
+            targetGroups: this.targetGroups,
+            medicamentsId: this.form.value.medicamentsId
+        };
+
+        this.boxesService.addBox(box);
+        this.router.navigate([Constants.PATH.dashboard]);
+
+    }
+}
