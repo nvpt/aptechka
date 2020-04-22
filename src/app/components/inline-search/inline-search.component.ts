8af901436ba1cd57a2f212278cf0b9dae58f77b5
Eugene_Z <evgeniy.zolotarev@firstlinesoftware.com>
2020-04-23 00:04:26 +0300

    inline-search.component fix

--- src/app/components/inline-search/inline-search.component.ts
+++ src/app/components/inline-search/inline-search.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Subject} from 'rxjs';
 
 @Component({
@@ -6,7 +6,7 @@ import {Subject} from 'rxjs';
     templateUrl: './inline-search.component.html',
     styleUrls: ['./inline-search.component.scss']
 })
-export class InlineSearchComponent implements OnInit, AfterContentInit {
+export class InlineSearchComponent implements OnInit {
     @ViewChild('inputField') inputField: ElementRef;
     @Input() search: string;
     @Output() searchChange: EventEmitter<string> = new EventEmitter<string>();
@@ -17,14 +17,6 @@ export class InlineSearchComponent implements OnInit, AfterContentInit {
     }
 
     ngOnInit(): void {
-
-    }
-
-    ngAfterContentInit(): void {
-        setTimeout(()=>{
-            console.log('20 >>> this.inputField.nativeElement: ', this.inputField);
-
-        },0)
 
     }
 
